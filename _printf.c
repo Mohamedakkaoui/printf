642f8277a42aab54272799a208b1cfae221d4a32
code0wizard <140090259+code0wizard@users.noreply.github.com>
2023-11-11 21:12:15 +0100

    fixed

--- _printf.c
+++ _printf.c
@@ -3,52 +3,52 @@
 #include <stddef.h>
 /**
  * _printf - function tat produces output according to a format
- * 
+ *
  * @format : a character string
- * 
+ *
  * Return : number of charcaters printed
-*/
+ */
 int _printf(const char *format, ...)
 {
-  va_list print;
-  int x;
-  char *b;
-  va_start(print , format);
-    if (format == NULL)
-    {
-      return(-1);
-    }
-  
-   for (x = 0; *format; x++)
-    {
-        if (*format != '%')
-        {
-            _putchar(*format);
-        }
-        else
-        {
-          format++;
-        
-        	if (*format == 'c')
-        	{
-          		_putchar(va_arg(print, int));      
-        	}
-        	else if (*format == 's')
-        	{
-          		b = va_arg(print, char *);
-          	while (*b != '\0')
-          	{
-            		_putchar(*b);
-            		b++;
-          	}
-        	}
-        	else if(*format == '%')
-        	{
-          		_putchar('%');
-        	}
+	va_list print;
+	int x;
+	char *b;
+
+	va_start(print , format);
+	if (format == NULL)
+	{
+		return(-1);
 	}
-        format++;
-    }
-  va_end(print);
-  return (x);
+	for (x = 0; *format; x++)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+		}
+		else
+		{
+			format++;
+
+			if (*format == 'c')
+			{
+				_putchar(va_arg(print, int));      
+			}
+			else if (*format == 's')
+			{
+				b = va_arg(print, char *);
+				while (*b != '\0')
+				{
+					_putchar(*b);
+					b++;
+				}
+			}
+			else if(*format == '%')
+			{
+				_putchar('%');
+			}
+		}
+		format++;
+	}
+	va_end(print);
+	return (x);
 }
