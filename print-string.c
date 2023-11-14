7b119fbfffe2a6ecb158469fe3c05582c6ce7a49
code0wizard <140090259+code0wizard@users.noreply.github.com>
2023-11-14 02:26:36 +0100

    function that prints a string

---
+++ print-string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+/** print-string - prints a string
+ * @print : list of argument
+ *Return: length of the string
+ */
+int print_string(va_list print)
+{
+	int i, length;
+	char *str;
+
+	str = va_arg(print, char *);
+	if (str == NULL)
+	{
+		i = 0;
+		str = "(null)";
+		length = _strlen(str);
+		while (i < length)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		return (length);
+	}
+	else
+	{
+		i = 0;
+		length  = _strlen(str);
+		while (i < length)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		return (length);
+	}
+}
