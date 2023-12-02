b7c3a601738cf7b67659c4c00c3891625870329b
code0wizard <140090259+code0wizard@users.noreply.github.com>
2023-12-02 21:33:35 +0100

    fixed

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
+typedef struct function
+{
+	char *spec;
+	int (*function)();
+}search;
+
+int _strlen(char *str);
+int print_char(va_list print);
+int print_perc(void);
+int print_string(char *);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
